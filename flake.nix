26677b11c0506601d9449936d30ffb2c7d222800
Andrei Volt <andrei@avolt.net>
2026-03-03 17:51:58 +0100

    add nix flake for building as chromium extension

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      forAllSystems = nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" ];
+    in {
+      packages = forAllSystems (system:
+        let pkgs = import nixpkgs { inherit system; };
+        in {
+          default = pkgs.buildNpmPackage {
+            pname = "refined-hacker-news";
+            version = "0.0.1";
+            src = self;
+
+            npmDepsHash = "sha256-eMpSQQTYJgInbFaK/XHX0COOMuSoTJCEGvs7h/Hob9c=";
+            makeCacheWritable = true;
+            npmFlags = [ "--ignore-scripts" ];
+
+            buildPhase = ''
+              runHook preBuild
+              npx webpack --mode=production
+              npx stylus ./src/*.styl --out ./dist/ --compress
+              runHook postBuild
+            '';
+
+            installPhase = ''
+              runHook preInstall
+              mkdir -p $out/share/chromium-extension
+              cp -r dist/* $out/share/chromium-extension/
+              runHook postInstall
+            '';
+
+            dontNpmInstall = true;
+          };
+        }
+      );
+    };
+}
