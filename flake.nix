42d05efb7c41d73fc7b40e3436096bc07d15167a
Andrei Volt <andrei@avolt.net>
2026-03-07 17:22:49 +0100

    self-contained CRX signing via PEM key in flake

--- flake.nix
+++ flake.nix
@@ -8,9 +8,10 @@
       forAllSystems = nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" ];
     in {
       packages = forAllSystems (system:
-        let pkgs = import nixpkgs { inherit system; };
-        in {
-          default = pkgs.buildNpmPackage {
+        let
+          pkgs = import nixpkgs { inherit system; };
+
+          extension = pkgs.buildNpmPackage {
             pname = "refined-hacker-news";
             version = "0.0.1";
             src = self;
@@ -35,6 +36,30 @@
 
             dontNpmInstall = true;
           };
+
+          manifest = builtins.fromJSON (builtins.readFile "${extension}/share/chromium-extension/manifest.json");
+
+          extId = builtins.readFile (pkgs.runCommand "refined-hacker-news-ext-id" {
+            nativeBuildInputs = [ pkgs.python3 pkgs.openssl ];
+          } ''
+            python3 ${./nix/crx-id.py} ${./keys/signing.pem} > $out
+          '');
+
+          crx = pkgs.runCommand "refined-hacker-news-crx" {
+            nativeBuildInputs = [ pkgs.python3 pkgs.openssl ];
+          } ''
+            mkdir -p $out
+            python3 ${./nix/pack-crx3.py} ${extension}/share/chromium-extension ${./keys/signing.pem} $out/extension.crx
+          '';
+        in {
+          default = pkgs.linkFarm "refined-hacker-news" [
+            { name = "share/chromium/extensions/${extId}.json";
+              path = pkgs.writeText "${extId}.json" (builtins.toJSON {
+                external_crx = "${crx}/extension.crx";
+                external_version = manifest.version;
+              });
+            }
+          ];
         }
       );
     };
